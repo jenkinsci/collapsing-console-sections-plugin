8bb2ff65cf06a291ba705b178232a0204d4de7a6
Oleg Nenashev <nenashev@synopsys.com>
2013-10-26 10:31:30 +0400

    Added contributors to license notes
    Signed-off-by: Oleg Nenashev <o.v.nenashev@gmail.com>
    Signed-off-by: Oleg Nenashev <nenashev@synopsys.com>

--- src/main/java/org/jvnet/hudson/plugins/collapsingconsolesections/CollapsingSectionAnnotator.java
+++ src/main/java/org/jvnet/hudson/plugins/collapsingconsolesections/CollapsingSectionAnnotator.java
@@ -1,7 +1,7 @@
 /*
  * The MIT License
  *
- * Copyright (c) 2010, Yahoo! Inc.
+ * Copyright (c) 2010, Yahoo! Inc. and contributors
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
